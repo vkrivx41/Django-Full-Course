99b7100255a641a1dcb9fd086ce867eed76413fd
Pasxal Le Grand <vkrivx41@gmail.com>
2026-02-08 18:17:26 +0200

    2. Rest Framework - Configuration and Setup

---
+++ 2. Intermediate/5. REST Framework/1. Configuration and Setup/api/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from uuid import uuid4
+
+
+class User(AbstractUser):
+    pass
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def in_stock(self) -> bool:
+        return self.stock > 0
+    
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "Pending"
+        CONFIRMED = "Confirmed"
+        CANCELLED = "Cancelled"
+
+    order_id = models.UUIDField(primary_key=True, default=uuid4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    product = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=30,
+        choices=StatusChoices.choices,
+        default=StatusChoices.PENDING
+    )
+
+    def __str__(self):
+        return f"Order {self.order_id} by {self.user.username}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in Order {self.order.order_id}"
+    
+    @property
+    def item_subtotal(self) -> float:
+        return self.product.price * self.quantity
+    
