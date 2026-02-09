79beb5af5bff049f475a591a08eb486f6f42497a
Pasxal Le Grand <vkrivx41@gmail.com>
2026-02-09 18:05:59 +0200

    2. Rest Framework - APIView class

---
+++ 2. Intermediate/5. REST Framework/9. APIView class/api/views.py
@@ -0,0 +1,67 @@
+from django.db.models import Avg
+
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from api.serializers import ProductSerializer, OrderSerializer, ProductsInfoSerializer
+from api.models import Product, Order, OrderItem
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects.prefetch_related(
+        'orders', 'items'
+    ).all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailAPIView(RetrieveAPIView):
+    queryset = Product.objects.prefetch_related('orders')
+    serializer_class = ProductSerializer
+
+    # lookup_field = 'id'  # the field name to lookup for products/<int:id>
+    # lookup_field = 'name'  # the field name to lookup for products/<str:name>
+
+    lookup_url_kwarg = 'product_id'  # the name of the dynamic url lookup products/<int:product_id>
+
+
+class OrderListApiView(ListAPIView):
+    queryset = Order.objects.prefetch_related(
+        'items', 'items__product'
+    ).all()
+    serializer_class = OrderSerializer
+
+
+class UserOrderListApiView(ListAPIView):
+    queryset = Order.objects.prefetch_related(
+        'items', 'items__product'
+    ).all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Overwrite the queryset to filter only the orders belonging to the logged in user
+        :param self: refers back to the API view, which also contain the request
+        """
+        user = self.request.user
+        if not user.is_authenticated:
+            return []
+
+        qs = super().get_queryset()
+
+        return qs.filter(user=user)
+
+
+class ProductInfoAPIView(APIView):
+    def get(self, request):
+        products = Product.objects.prefetch_related('orders').all()
+
+        serializer = ProductsInfoSerializer({
+            'products': products,
+            'count': products.count(),
+            'avg_price': products.aggregate(avg=Avg('price'))['avg']
+        })
+
+        return Response(serializer.data)
