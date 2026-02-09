b4dad53bee861bd5c9af38f8f56dc22c03624542
Pasxal Le Grand <vkrivx41@gmail.com>
2026-02-09 17:48:03 +0200

    2. Rest Framework - Dynamic Filtering Overriding get_queryset() method

---
+++ 2. Intermediate/5. REST Framework/7. Dynamic Filtering Overriding get_queryset() method/api/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from api.models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline
+    ]
+
+admin.site.register(Order, OrderAdmin)
