d6757a6ab1fd3083f3847d1c0fbd91116df3d36e
Pasxal Le Grand <vkrivx41@gmail.com>
2026-02-11 11:17:42 +0200

    2. Rest Framework - JWT Authentication with djangorestframework-simplejwt

---
+++ 2. Intermediate/5. REST Framework/12. JWT Authentication with djangorestframework-simplejwt/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from api.views import *
+from api.serializers import OrderSerializer
+
+
+app_name: str = "api"
+
+
+urlpatterns = [
+    path("products/", ProductListCreateAPIView.as_view(), name="products"),
+    path("products/create", ProductCreateAPIView.as_view(), name="products_create"),
+    path("products/<int:product_id>", ProductDetailAPIView.as_view(), name="product"),
+    path("products/info", ProductInfoAPIView.as_view(), name="products_info"),
+    path("orders/", OrderListApiView.as_view(
+        serializer_class=OrderSerializer
+    ), name="orders"),
+    path("user-orders/", UserOrderListApiView.as_view(), name="user_orders"),
+]
